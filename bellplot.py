2b5316e1b783a1d96f5743a34d22c1c6652ee6ae
Wesley Neill <wesley.neill@gmail.com>
2019-10-16 14:19:00 -0700

    improved plots for all models

--- bellplot.py
+++ bellplot.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import math
 import rungekutta4 as rk
 
 
@@ -9,53 +7,36 @@ if __name__ == '__main__':
 
     # the ODE y' = -t * y, which has solution y = exp(-t^2 / 2)
     def field(t, vect):
-        y = vect
-        return np.array([-t * y])
+        return -t * vect
 
+    def p(t):
+        return np.exp(-t ** 2 / 2)
 
     # Set the interval over which we want a solution.
     t_0 = -10
     t_n = 10
-    dt = .05
 
-    # Determine number of steps in accordance with mesh size
-    steps = int((t_n - t_0) / dt)
-    time = np.linspace(t_0, t_n, steps, endpoint=False)
-    # time = np.arange(t_0, t_n, dt)
+    # Set initial conditions
+    x_0 = np.exp(-t_0 ** 2 / 2)
 
-    # Initialize solution vectors and error collection
-    x = np.zeros(steps)
-    error = np.zeros(steps)
-    x[0] = 1.928749848e-22
-    error[0] = 0
+    fig, axs = plt.subplots(2, 1, figsize=(10, 6))
 
-    for i in range(1, steps):
-        x[i] = rk.rk4(dt, time[i-1], field, x[i-1])
-        error[i] = abs(x[i] - math.pow(math.e, (-time[i] ** 2) / 2)) / math.pow(math.e, (-time[i] ** 2) / 2)
-
-
-    # collect solution data for plotting
-    df = pd.DataFrame({'t': time, 'x': x})
-
-    edf = pd.DataFrame({'error':error, 'time':time})
-
-    print("max error:", max(edf['error'].to_numpy()))
-
-    fig, axs = plt.subplots(2,1, figsize=(10, 6), sharex=True)
-    color = 'green'
     axs[0].set_ylabel(r'$y(t) = e^{-t^2 / 2}$')
-    axs[0].set_xlabel('Time')
-    axs[0].plot('t', 'x', data=df, color=color)
-    axis = plt.gca()
-    axis.set_ylim((0, 2))
-
+    axs[1].set_xlabel('t')
     axs[1].set_ylabel('Relative Error')
-    axs[1].plot('time', 'error', data=edf, color='red')
-    plt.grid(True)
-    plt.autoscale(axis='y')
+
+    for delta in [0.1, 0.05, 0.025, .01, .005]:
+        # Solve for given time step
+        time = np.arange(t_0, t_n, delta)
+        x = rk.rk4(delta, time, field, x_0)
+        error = abs((x - np.exp((-time ** 2) / 2)) / (np.exp((-time ** 2) / 2)))
+
+        # Plot result and error
+        axs[0].plot(time, x, label=r"$\Delta t=$%.3f" % delta)
+        axs[1].plot(time, error, label=r"$\Delta t=$%.3f" % delta)
+
+        for iax in axs:
+            iax.grid()
+            iax.legend()
+
     plt.show()
-
-
-
-
-
--- rungekutta4.py
+++ rungekutta4.py
@@ -3,15 +3,28 @@ import pandas as pd
 import matplotlib.pylab as plt
 import time as tm
 
+def rk4(dt, t, field, y_0):
+    """
+    :param dt: float - the timestep
+    :param t: array - the time mesh
+    :param field: method - the vector field y' = f(t, y)
+    :param y_0: array - containains initial conditions
+    :return: ndarray - solution
+    """
 
-def rk4(dt, t, field, y_n):
+    # solution matrix, solutions are row vectors
+    y = np.asarray(len(t) * [y_0])
 
-    k1 = dt * field(t, y_n)
-    k2 = dt * field(t + 0.5 * dt, y_n + 0.5 * k1)
-    k3 = dt * field(t + 0.5 * dt, y_n + 0.5 * k2)
-    k4 = dt * field(t + 0.5 * dt, y_n + k3)
+    for i in np.arange(len(t) - 1):
+        k1 = dt * field(t[i], y[i])
+        k2 = dt * field(t[i] + 0.5 * dt, y[i] + 0.5 * k1)
+        k3 = dt * field(t[i] + 0.5 * dt, y[i] + 0.5 * k2)
+        k4 = dt * field(t[i] + 0.5 * dt, y[i] + k3)
+        res = y[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        y[i + 1] = y[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
-    return y_n + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+    return y
 
 
 if __name__ == '__main__':
@@ -50,17 +63,15 @@ if __name__ == '__main__':
     time = np.arange(t_0, t_n, Dt)
 
     # Initialize solution vector and initial conditions
-    x = np.zeros((3, steps))
-    x[:, 0] = [1e-16, .075 * K_s, 1.]
+    x_0 = [1e-16, .075 * K_s, 1.]
 
     # Solve the system of equations
     start = tm.time()
-    for i in np.arange(1, steps):
-        x[:, i] = rk4(Dt, time[i - 1], system, x[:, i - 1])
+    x = rk4(Dt, time, system, x_0)
     print("runtime was", tm.time() - start, "seconds")
 
     # Create Pandas DataFrame
-    df = pd.DataFrame(x, index=['B', 'S', 'E'], columns=time)
+    df = pd.DataFrame(x, index=time, columns=['B', 'S', 'E'])
 
     fig, ax1 = plt.subplots(figsize=(10, 6))
     fig.subplots_adjust(right=0.75)
@@ -68,20 +79,20 @@ if __name__ == '__main__':
     color = 'red'
     ax1.set_xlabel('Years')
     ax1.set_ylabel('Budworm Population Density', color=color)
-    ax1.plot(time, df.loc['B', :], color=color)
+    ax1.plot(time, df.loc[:,'B'], color=color)
     ax1.tick_params(axis='y', labelcolor=color)
 
     color = 'green'
     ax2 = ax1.twinx()
     ax2.set_ylabel('Foliage Density', color=color)
-    ax2.plot(time, df.loc['S', :], color=color)
+    ax2.plot(time, df.loc[:,'S'], color=color)
     ax2.tick_params(axis='y', labelcolor=color)
 
     color = 'blue'
     ax3 = ax1.twinx()
     ax3.spines["right"].set_position(("axes", 1.2))
     ax3.set_ylabel('Foliage Health', color=color)
-    ax3.plot(time, df.loc['E', :], color=color)
+    ax3.plot(time, df.loc[:, 'E'], color=color)
     ax3.tick_params(axis='y', labelcolor=color)
 
     plt.show()
